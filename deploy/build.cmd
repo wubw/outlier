6eadf93e0fa647e35841721e69f0223d108caac6
Wu <Bevan.Wu@dnvgl.com>
2018-03-25 21:43:07 +0200

    local docker env

---
+++ deploy/build.cmd
@@ -0,0 +1,14 @@
+cd ..
+
+cd frontend
+call npm install
+call ng build --env=test --output-path ..\backend\outlier.api\wwwroot
+cd ..
+
+cd backend
+cd outlier.api
+dotnet publish -c Release -o ./obj/Docker/publish
+cd ..
+cd ..
+
+cd deploy
